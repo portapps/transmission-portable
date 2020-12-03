1e1671114699d3fe6d411f4edf1aebf518164dea
CrazyMax <crazy-max@users.noreply.github.com>
2020-12-03 13:21:42 +0100

    Portapps 3.0.0

--- main.go
+++ main.go
@@ -6,9 +6,9 @@ import (
 	"os"
 	"path"
 
-	"github.com/portapps/portapps/v2"
-	"github.com/portapps/portapps/v2/pkg/log"
-	"github.com/portapps/portapps/v2/pkg/utl"
+	"github.com/portapps/portapps/v3"
+	"github.com/portapps/portapps/v3/pkg/log"
+	"github.com/portapps/portapps/v3/pkg/utl"
 )
 
 type config struct {
@@ -47,7 +47,7 @@ func main() {
 		}()
 	}
 
-	utl.OverrideEnv("TRANSMISSION_HOME", app.DataPath)
+	os.Setenv("TRANSMISSION_HOME", app.DataPath)
 
 	defer app.Close()
 	app.Launch(os.Args[1:])
